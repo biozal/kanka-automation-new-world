017f1532058a0b0bbd30e9180b5d0d428a520cc9
Aaron LaBeau <alabeau@gmail.com>
2024-08-02 22:41:27 -0500

    feat: added url's for other APIS

--- bin/config.js
+++ bin/config.js
@@ -1,9 +1,20 @@
 const mainUrl = 'https://api.kanka.io/1.0/';
 const campaignUrl = `${mainUrl}campaigns/`;
+const locationsUrl = `${mainUrl}locations/`;
+const familiesUrl = `${mainUrl}families/`;
+const charactersUrl = `${mainUrl}characters/`;
+const itemsUrl = `${mainUrl}items/`;
+const organisationsUrl = `${mainUrl}organisations/`;
+
 const token = process.env.KANKA_API;
 
 module.exports = {
    mainUrl,
    campaignUrl,
+   locationsUrl,
+   familiesUrl,
+   charactersUrl,
+   itemsUrl,
+   organisationsUrl,
    token 
 };
